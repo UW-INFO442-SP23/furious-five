7eeacd97c4fee0cbd54991881e36416713d9134e
fdineh <fdineh@uw.edu>
2023-04-24 17:59:28 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+
+
+export default function App(props) {
+    return (
+      <div>
+        <Router>
+          <Routes>
+  
+          </Routes>
+        </Router> 
+      </div>
+    );
+  }
+  
+
