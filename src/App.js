10178c6711fbd597f27248ab601c43f399f71f3d
trangtran10 <91641138+trangtran10@users.noreply.github.com>
2023-04-26 14:43:42 -0700

    created components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import NavBar from './components/NavBar/navbar.js'
+import Form from './components/Form/form.js'
+import Home from './components/Home/home.js'
+import Login from './components/Login/login.js'
+import Posts from './components/Posts/posts.js'
 
 
 
@@ -9,6 +14,7 @@ export default function App(props) {
       <div>
         <Router>
           <Routes>
+
   
           </Routes>
         </Router> 
