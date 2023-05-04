c5049a5c2f7d2cc253c87c3c212234da3921cfdf
trangtran10 <91641138+trangtran10@users.noreply.github.com>
2023-05-03 18:37:05 -0700

    homepage edits

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -7,19 +7,21 @@ export default function Home(props) {
 
             <section className="about">
             <h1 className="about">FOODLINK</h1>
-            <p className="about">Tagline/Mission statement. </p>
+            <p className="about">A space for restaraunts to connect with non-profit organizations aiming to reduce food waste. </p>
             </section>
 
             <section className="community">
+                <img className="community" src="./components/img/community.jpg" alt="restaurant building"/>
                 <h2 className="community">COMMUNITY</h2>
                 <p className="community">Find food using our forum and collect extra inventory items from restaurants near you.</p>
-                <button Link to="/Posts" className="btn btn-outline-primary">Forum</button>
+                <button Link to="/Posts" className="button communitybtn">Forum</button>
             </section>
 
             <section className="contribute">
+                <img className="contribute" src="./components/img/contribute.jpg" alt="various food dishes on a table"/>
                 <h2 className="contribute">CONTRIBUTE</h2>
                 <p className="contribute"> Own a restaurant and want to contribute to reducing food waste? Join and post your remaining food inventory.</p>
-                <button Link to="/Form" className="btn btn-outline-primary2">Create a post</button>
+                <button Link to="/Form" className="button contributebtn">Create a post</button>
             </section>
 
         </div>
