e7f559077ba1f8a11b3525d96b65175b53934d12
trangtran10 <91641138+trangtran10@users.noreply.github.com>
2023-05-10 11:34:27 -0700

    nav additions

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function NavBar(props) {
-    
+    return (
+        <div className="navigator">
+            <li><a className="navigator" href="/Home">Home</a></li>
+            <li><a className="navigator" href="/Posts">Forum</a></li>
+            <li><a className="navigator" href="/Form">Create a post</a></li>
+        </div>
+    )
 }
