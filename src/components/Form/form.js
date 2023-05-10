e8d41a88431ab0d341ad2aff2dd51429426aec9b
fdineh <fdineh@uw.edu>
2023-05-10 15:53:50 -0700

    erase

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -6,30 +6,5 @@ import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 export default class Form extends React.Component{
-  state = {
-    RestaurantName: "",
-    EmailAddress: "",
-    Address: "",
-    Cuisine: "",
-    Menu: ""
 
-  };
-  
-  render() {
-    return(
-      <div className="form-container">
-        <div className='small-form'>
-      <form>
-        <h2 className="form-title">Form</h2>
-        <input type="text" className= "form-text" placeholder='Restaurant Name' value={this.state.RestaurantName} /> <br />
-        <input type="text" className= "form-text" placeholder='Email Address' value={this.state.RestaurantName} /> <br />
-        <input type="text" className= "form-text" placeholder='Address' value={this.state.RestaurantName} /> <br />
-        <input type="text" className= "form-text" placeholder='Cuisine' value={this.state.RestaurantName} /> <br />
-        <input type="text" className= "form-text" placeholder='Menu' value={this.state.RestaurantName} /> <br />
-        <button>Submit</button>
-      </form>
-      </div>
-      </div>
-    )
-  }
 }
