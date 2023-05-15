736ad30ffcea208b17c4f2295234d44a9b1dbfc9
YuWenSu-Bianca <biancakatesubr@gmail.com>
2023-05-14 23:52:48 -0700

    editing footer layout

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer(props) {
     return (
-        <div className="container-fluid text-center" style={{backgroundColor: "3D3E3C"}}>
+        <div className="container-fluid" style={{backgroundColor: "#3D3E3C"}}>
             <div className="row" style={{paddingTop: "20px"}}>
-                <div style={{paddingLeft: "40px"}}>
+                <div style={{paddingLeft: "40px", color: "white"}}>
                     <h5>Contact Us</h5>
                     <span>Email</span>
                     <br></br>
