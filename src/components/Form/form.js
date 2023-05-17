44570c3634720aad27a73448b246b6ffe105ce32
Eivy1234 <eivy1234@uw.edu>
2023-05-17 11:34:03 -0700

    add js & css code for form page

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export default function Form(props) {
-    
+function Form() {
+  return (
+    <div className="create-post-container">
+
+      {/* Form */}
+      <form>
+        <label htmlFor="title">Restaurant Name:</label>
+        <input type="text" id="title" name="title" required />
+        <label htmlFor="email">Email Address:</label>
+        <input type="text" id="email" name="email" required />
+        <label htmlFor="address">Address:</label>
+        <input type="text" id="address" name="address" required />
+        <label htmlFor="content">Cuisine:</label>
+        <textarea id="content" name="content" rows="10" required></textarea>
+
+        <label htmlFor="menu">Menu:</label>
+        <input type="file" id="menu" name="menu" />
+
+        <button type="submit">Post</button>
+      </form>
+
+    </div>
+  );
 }
+
+export default Form;
