200965b23fd7b3b0517f55a8e9448bac061bcd77
Mehr Mehta <mmehta@uw.edu>
2023-05-21 21:23:04 -0700

    done

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,89 +1,48 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import { ArrowBack, ArrowForwardOutlined } from "@mui/icons-material";
-import { Button, IconButton } from "@mui/material";
+import React, { useState } from 'react';
 
-const CircledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-radius: 25px;
-  width: 100%;
-  border: 3px solid gray;
-  margin-right: 1rem;
-`;
-const Text1 = styled.div`
-  font-size: 22px;
-  color: gray;
-  margin-left: auto;
-  margin-right: auto;
-`;
-const Text2 = styled.div`
-  font-size: 22px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Login = (props) => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showStatus, setShowStatus] = useState(false);
-  const [statusModalTitle, setStatusModalTItle] = useState("");
-  const [statusModalDescription, setStatusModalDescription] = useState("");
-  const setIsLoggedIn = props.setIsLoggedIn;
-  const setAccountInfo = props.setAccountInfo;
-  const navigate = useNavigate();
 
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-
-    const params = {
-      email: email,
-      password: password
-    };
-    
-    // const resp = await loginAccount(params);
-    // if (!resp.status) {
-    //   setIsLoggedIn(true);
-    //   setAccountInfo(resp);
-    //   navigate(HOME_ROUTE);
-    // } else {
-    //   setStatusModalTItle("Error Logging in");
-    //   setStatusModalDescription(TRY_AGAIN_LATER_MSG);
-    //   setShowStatus(true);
-    // }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handleCreateAccount = () => {
-    props.setDisplay("createAccount");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic here with email and password
   };
 
   return (
     <div>
-      <CircledContainer className="p-3">
-        <IconButton className="bg-black p-1 align-self-start" aria-label="back" onClick={() => props.setDisplay("")}>
-          <ArrowBack htmlColor="#FFFFFF" />
-        </IconButton>
-        <h2 className="text-center" style={{ fontSize: "22px" }}>Sign In</h2>
-        {/* showStatus ?
-          <StatusModal
-            title={statusModalTitle}
-            description={statusModalDescription}
-            open={showStatus}
-            handleClose={() => setShowStatus(false)}
-          /> :
-          "" */}
-        <form onSubmit={handleOnSubmit} className="d-flex flex-column">
-          <div className="form align-items-center">
-            <div className="form-group col-md-4">
-              <label htmlFor="exampleInputEmail1">Email *</label>
-              <input type="email" className="form-control " onChange={(e) => setEmail(e.target.value)} value={email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            </div>
-            <div className="form-group col-md-4">
-              <label htmlFor="exampleInputPassword1">Password *</label>
-              <input type="password" className="form-control " value={password} onChange={(e) => setPassword(e.target.value)} id="exampleInputPassword1" aria-describedby="passwordHelp" placeholder="Enter Password" />
-            </div>
-          </div>
-          <Text1>Don't have an account?</Text1>
-          <Button onClick={handleCreateAccount}><Text2>Tap here to
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
