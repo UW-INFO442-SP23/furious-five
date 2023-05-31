55843ecba3792d055577ae4db7314ce6a5da8a94
YuWenSu-Bianca <biancakatesubr@gmail.com>
2023-05-30 20:50:02 -0700

    fixed View Form UI and added media queries for view form page

--- src/components/Form/viewposts.js
+++ src/components/Form/viewposts.js
@@ -16,23 +16,23 @@ export default function ViewPosts(props) {
             </div>
 
             <div>
-                <div class="container-fluid" style={{paddingRight: "150px", paddingLeft: "150px"}}>
-                    <div class="card" style={{margin: "10px", borderRadius: "20px", boxShadow: "10px black"}}>
-                        <img class="card-img-top"src="" alt="PHOTO" style={{padding: "90px", textAlign: "center"}}/>
-                        <div class="card-body" style={{padding: "40px"}}>
-                            <p class="card-text" style={{fontSize: "30px"}}><b>Restaurant Name</b></p>
-                            <p class="card-text" style={{fontSize: "20px"}}>Date Posted</p>
-                            <hr style={{border: "1px solid"}}></hr>
-                            <p class="card-text">Email:</p>
-                            <p class="card-text">Cuisine:</p>
-                            <p class="card-text">Food Intentory List:</p>
-                            <div class="card" style={{color: "D9D9D9", margin: "20px"}}>
-                                <p class="card-text" style={{margin: "100px", textAlign: "center"}}>Food Inventory Table</p>
+                <div class="view-post-card">
+                        <div class="view-card">
+                            <img class="view-photo" src="" alt="PHOTO"/>
+                            <div class="view-body">
+                                <p class="card-text" style={{fontSize: "30px"}}><b>Restaurant Name</b></p>
+                                <p class="card-text" style={{fontSize: "20px"}}>Date Posted</p>
+                                <hr style={{border: "1px solid"}}></hr>
+                                <p class="card-text">Email:</p>
+                                <p class="card-text">Cuisine:</p>
+                                <p class="card-text">Food Intentory List:</p>
+                                <div class="inventory-display" >
+                                    <p class="card-text">Food Inventory Table</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
 
     )
