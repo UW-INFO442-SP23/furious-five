36e68ad6d5d9d55ce27c9606045a758238517ecd
fdineh <fdineh@uw.edu>
2023-05-31 14:05:35 -0700

    learn

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -14,7 +14,7 @@ export default function Home(props) {
                 <h2 className="community">COMMUNITY</h2>
                 <p className="community">Find food using our forum and collect extra inventory items from restaurants near you.</p>
                 <a href="/Forum">
-                    <button className="button communitybtn">Forum</button>
+                    <button className="button contributebtn">Forum</button>
                 </a>
             </section>
 
@@ -28,8 +28,14 @@ export default function Home(props) {
             </section>
 
             <section className="video">
+            <h2 className="video">LEARN MORE</h2>
+                <p>Seattle Public Utilities estimates that every family in the city throws away some 400 pounds 
+                    of food each year. To help reduce food waste in Seattle, can you donate your extra supply 
+                    of packaged foods to a local food pantry or a food drive. You can check your fridge often 
+                    to track so you know what items to eat. You can freeze food from 3 to 6 months if the food 
+                    is wrapped correctly. 
+                </p>
                 <iframe align="center" src="https://www.youtube.com/embed/ishA6kry8nc"></iframe>
-                <h2 className="video">LEARN MORE</h2>
                 <p className="video"> Watch this video to learn more about the impact of food waste.</p>
                 <p>Have questions about food regulations?
                 <a href="https://www.fda.gov/food/guidance-regulation-food-and-dietary-supplements"> See FDA regulations</a></p> 
