d4a895e1bd2a362fc6bd9ddad13102062bc6dabe
Eivy1234 <eivy1234@uw.edu>
2023-05-30 21:39:00 -0700

    adjusted viewpost UI

--- src/components/Form/viewposts.js
+++ src/components/Form/viewposts.js
@@ -1,39 +1,30 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ViewPosts(props) {
-    return(
-        <div className="home-container" style={{paddingBottom: "20px"}}>
+    return (
+        <div className="container">
+            <div className="post-header">
+                <h1 className="post-title">Food Post</h1>
+            </div>
 
-            <div className="formheader">
-                <div className="container">
-                    <div class="p-2">
-                        <section className="post">
-                            <h1 className="post">Food Post</h1>
-                        </section>
+            <div className="post-card">
+                <div className="post-image">
+                    <img className="card-img-top" src="" alt="PHOTO" />
+                </div>
+                <div className="post-details">
+                    <p className="restaurant-name">Restaurant Name</p>
+                    <p className="date-posted">Date Posted</p>
+                    <hr className="divider" />
+                    <p className="post-info">Email:</p>
+                    <p className="post-info">Cuisine:</p>
+                    <p className="post-info">Food Inventory List:</p>
+                    <div className="food-inventory-table">
+                        <p className="table-placeholder">Food Inventory Table</p>
                     </div>
                 </div>
             </div>
-
-            <div>
-                <div class="view-post-card">
-                        <div class="view-card">
-                            <img class="view-photo" src="" alt="PHOTO"/>
-                            <div class="view-body">
-                                <p class="card-text" style={{fontSize: "30px"}}><b>Restaurant Name</b></p>
-                                <p class="card-text" style={{fontSize: "20px"}}>Date Posted</p>
-                                <hr style={{border: "1px solid"}}></hr>
-                                <p class="card-text">Email:</p>
-                                <p class="card-text">Cuisine:</p>
-                                <p class="card-text">Food Intentory List:</p>
-                                <div class="inventory-display" >
-                                    <p class="card-text">Food Inventory Table</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
         </div>
-
-    )
+    );
 }
